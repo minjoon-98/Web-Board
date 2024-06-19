8dbd9b37c09658fd9349c443a94e84e77b9b78fd
unknown <kmj54321@cau.ac.kr>
2024-06-20 00:39:39 +0900

    add: modify&delete answer

--- src/main/java/com/example/board/Answer/AnswerService.java
+++ src/main/java/com/example/board/Answer/AnswerService.java
@@ -1,11 +1,13 @@
 package com.example.board.Answer;
 
+import com.example.board.DataNotFoundException;
 import com.example.board.Question.Question;
 import com.example.board.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -20,5 +22,24 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setAuthor(author);
         this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
