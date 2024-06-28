d5182ce4d4850a32a07713f5fdb5509bd8a4beeb
minjoon-98 <4kmj54321@gmail.com>
2024-06-28 18:11:44 +0900

    fix: blank exception

--- src/main/java/com/example/board/Question/QuestionForm.java
+++ src/main/java/com/example/board/Question/QuestionForm.java
@@ -1,5 +1,6 @@
 package com.example.board.Question;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -10,11 +11,13 @@ import lombok.Setter;
 @Setter
 public class QuestionForm {
     @NotEmpty(message = "제목은 필수항목입니다.")
-    @Pattern(regexp = "^(?=.*\\S).*$", message = "제목을 입력해주세요.")
+    @NotBlank
+//    @Pattern(regexp = "^(?=.*\\S).*$", message = "제목을 입력해주세요.")
     @Size(max = 200)
     private String subject;
 
     @NotEmpty(message = "내용은 필수항목입니다.")
-    @Pattern(regexp = "^(?=.*\\S).*$", message = "내용을 입력해주세요.")
+    @NotBlank
+//    @Pattern(regexp = "^(?=.*\\S).*$", message = "내용을 입력해주세요.")
     private String content;
 }
